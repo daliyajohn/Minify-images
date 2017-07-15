2f2e44907d2dfc8ed1964d27834c8a6ce96fdbaf
daliyajohn <daliyajohn75@gmail.com>
2017-07-15 16:45:08 +0530

    Add image minify code

---
+++ index.js
@@ -0,0 +1,12 @@
+const imagemin = require('imagemin');
+const imageminJpegtran = require('imagemin-jpegtran');
+const imageminPngquant = require('imagemin-pngquant');
+ 
+imagemin(['Exterior_4.jpg'], 'image/', {
+    plugins: [
+      imageminJpegtran(),
+      imageminPngquant({quality: '65-70'})
+    ]
+}).then(files => {
+    console.log(files);
+});
